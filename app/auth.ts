2094c32adf7abb33ce605334fac4f79c7688ecb9
Hyunwoo Seo <shw4008@gmail.com>
2024-01-11 00:41:21 +0900

    feat: add login page, absolute importing, changed signin button to move to login page

--- app/auth.ts
+++ app/auth.ts
@@ -12,6 +12,6 @@ export const {
     })
   ],
   pages: {
-    signIn: '/sign-in'
+    signIn: '/login'
   }
 });
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -113,7 +113,7 @@ export default function Navbar({ user }: { user: any }) {
                                 active ? 'bg-gray-100' : '',
                                 'flex w-full px-4 py-2 text-sm text-gray-700'
                               )}
-                              onClick={() => signIn('github')}
+                              onClick={() => signIn()}
                             >
                               Sign in
                             </button>
@@ -190,7 +190,7 @@ export default function Navbar({ user }: { user: any }) {
               ) : (
                 <div className="mt-3 space-y-1">
                   <button
-                    onClick={() => signIn('github')}
+                    onClick={() => signIn()}
                     className="flex w-full px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
                   >
                     Sign in
