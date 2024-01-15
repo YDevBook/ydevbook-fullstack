e22b10f714c5ca13dcbc7ba83f7c3bfcfa54a337
Hyunwoo Seo <shw4008@gmail.com>
2024-01-16 01:54:31 +0900

    feat: session user field update, main cta link logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,31 @@
+import { auth } from '@/auth';
+import { Button } from '@/components/atoms/Button';
+import { Text, Title } from '@tremor/react';
 import { sql } from '@vercel/postgres';
-import { Card, Title, Text } from '@tremor/react';
-import Search from './search';
-import UsersTable from './table';
-import { User } from '@/lib/definitions';
+import Link from 'next/link';
 
-export default async function IndexPage({
-  searchParams
-}: {
-  searchParams: { q: string };
-}) {
-  const search = searchParams.q ?? '';
-  const result = await sql`
-    SELECT id, name, email 
-    FROM users 
-    WHERE name ILIKE ${'%' + search + '%'};
-  `;
-  const users = result.rows as User[];
+export default async function IndexPage({}: {}) {
+  const session = await auth();
+  const isLoggedIn = !!session?.user;
+  let hasProfile = false;
+  if (isLoggedIn) {
+    const result = await sql`
+      SELECT COUNT(*) FROM profiles WHERE userId = ${session.user.id};
+    `;
+    hasProfile = result.rows[0].count !== '0';
+  }
+  const mainCTAHref =
+    isLoggedIn && hasProfile ? '/my-profile' : '/profile-form';
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Users</Title>
-      <Text>A list of users retrieved from a Postgres database.</Text>
-      <Search />
-      <Card className="mt-6">
-        <UsersTable users={users} />
-      </Card>
+      <Title>메인 랜딩 페이지</Title>
+      <Text>서비스 소개 및 랜딩 디자인, 메인 CTA 생성</Text>
+      <div className="flex flex-col space-y-2.5">
+        <Link href={mainCTAHref}>
+          <Button>프로필 생성 / 마이 프로필(메인 CTA)</Button>
+        </Link>
+      </div>
     </main>
   );
 }
--- auth.config.ts
+++ auth.config.ts
@@ -13,8 +13,27 @@ export const authConfig = {
         return Response.redirect(new URL('/', nextUrl.origin)); // 로그인 페이지에서 로그인이 되어있으면 홈으로 리다이렉트 (callBackUrl 에 따라 로직 추가 필요)
       } else {
         // 로그인 페이지가 아니면 (페이지에 따라 다른 로직 구성 필요)
+        const isOnProfilePage = nextUrl.pathname.startsWith('/profile-form');
+        if (isOnProfilePage) {
+          if (isLoggedIn) return true; // 프로필 페이지에서 로그인이 되어있으면 그대로 있기
+          return Response.redirect(new URL('/login', nextUrl.origin)); // 프로필 페이지에서 로그인이 안되어있으면 로그인 페이지로 리다이렉트
+        }
         return true;
       }
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.user = {
+        id: token.id as string,
+        name: token.name as string,
+        email: token.email as string
+      };
+      return session;
     }
   },
   providers: [] // Add providers with an empty array for now
