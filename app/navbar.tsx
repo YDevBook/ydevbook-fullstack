5aaefda411ef3f0912d3ca821c5e33d82c13f011
Hyunwoo Seo <shw4008@gmail.com>
2024-01-30 16:59:51 +0900

    feat: navbar 수정사항

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,11 +6,12 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import { signIn, signOut } from 'next-auth/react';
+import Link from 'next/link';
 
 const navigation = [
   { name: 'Home', href: '/' },
-  { name: 'Positions', href: '/positions' },
-  { name: 'Startup', href: '/startup' }
+  { name: '채용 공고', href: '/positions' },
+  { name: '스타트업 서비스', href: '/startup' }
 ];
 
 function classNames(...classes: string[]) {
@@ -29,33 +30,34 @@ export default function Navbar({ user }: { user: any }) {
             <div className="flex h-16 justify-between">
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center">
-                  <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    fill="none"
-                    className="text-gray-100"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <rect
-                      width="100%"
-                      height="100%"
-                      rx="16"
-                      fill="currentColor"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                      fill="black"
-                    />
-                  </svg>
+                  <Link href={'/'}>
+                    <svg
+                      width="32"
+                      height="32"
+                      viewBox="0 0 32 32"
+                      fill="none"
+                      className="text-gray-100"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        width="100%"
+                        height="100%"
+                        rx="16"
+                        fill="currentColor"
+                      />
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </Link>
                 </div>
                 <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
                   {navigation.map((item) => (
-                    <a
+                    <div
                       key={item.name}
-                      href={item.href}
                       className={classNames(
                         pathname === item.href
                           ? 'border-slate-500 text-gray-900'
@@ -64,8 +66,13 @@ export default function Navbar({ user }: { user: any }) {
                       )}
                       aria-current={pathname === item.href ? 'page' : undefined}
                     >
-                      {item.name}
-                    </a>
+                      <Link
+                        href={item.href}
+                        className="h-full inline-flex items-center"
+                      >
+                        {item.name}
+                      </Link>
+                    </div>
                   ))}
                 </div>
               </div>
@@ -107,7 +114,7 @@ export default function Navbar({ user }: { user: any }) {
                                 )}
                                 onClick={() => router.push('/my-profile')}
                               >
-                                My Profile
+                                내 프로필
                               </button>
                             )}
                           </Menu.Item>
@@ -120,7 +127,7 @@ export default function Navbar({ user }: { user: any }) {
                                 )}
                                 onClick={async () => await signOut()}
                               >
-                                Sign out
+                                로그아웃
                               </button>
                             )}
                           </Menu.Item>
@@ -135,7 +142,7 @@ export default function Navbar({ user }: { user: any }) {
                               )}
                               onClick={async () => await signIn()}
                             >
-                              Sign in
+                              로그인
                             </button>
                           )}
                         </Menu.Item>
@@ -203,13 +210,13 @@ export default function Navbar({ user }: { user: any }) {
                       onClick={() => router.push('/my-profile')}
                       className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
                     >
-                      My Profile
+                      내 프로필
                     </button>
                     <button
                       onClick={async () => await signOut()}
                       className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
                     >
-                      Sign out
+                      로그아웃
                     </button>
                   </div>
                 </>
@@ -219,7 +226,7 @@ export default function Navbar({ user }: { user: any }) {
                     onClick={async () => await signIn()}
                     className="flex w-full px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
                   >
-                    Sign in
+                    로그인
                   </button>
                 </div>
               )}
