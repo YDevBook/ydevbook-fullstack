5d00afef4573a7daa3eada2d16badc99d239f88b
Hyunwoo Seo <shw4008@gmail.com>
2024-02-01 14:19:25 +0900

    feat: add logo

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -7,6 +7,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import { signIn, signOut } from 'next-auth/react';
 import Link from 'next/link';
+import LogoImage from '@/assets/images/ydevbook_logo.webp';
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -31,27 +32,13 @@ export default function Navbar({ user }: { user: any }) {
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center">
                   <Link href={'/'}>
-                    <svg
-                      width="32"
-                      height="32"
-                      viewBox="0 0 32 32"
-                      fill="none"
-                      className="text-gray-100"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <rect
-                        width="100%"
-                        height="100%"
-                        rx="16"
-                        fill="currentColor"
-                      />
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                        fill="black"
-                      />
-                    </svg>
+                    <Image
+                      src={LogoImage}
+                      alt="YDevBook Logo"
+                      width={40}
+                      height={40}
+                      className="rounded-md"
+                    />
                   </Link>
                 </div>
                 <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
