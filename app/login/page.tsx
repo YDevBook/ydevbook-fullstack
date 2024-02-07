7a217468a262591a1ec6c8623319b325ae333fc2
Hyunwoo Seo <shw4008@gmail.com>
2024-02-07 19:19:33 +0900

    feat: login method only kakao fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,11 @@
 import LoginForm from '@/components/organisms/LoginForm';
+import LoginFormOnlyKakao from '@/components/organisms/LoginFormOnlyKakao';
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 md:-mt-32 p-4">
-        <LoginForm />
+      <div className="relative mx-auto w-full max-w-[640px] space-y-2.5 md:-mt-32 p-8">
+        <LoginFormOnlyKakao />
       </div>
     </main>
   );
