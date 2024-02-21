55e7569dd228558cb34294ef7311eda2a1e0459e
Hyunwoo Seo <shw4008@gmail.com>
2024-02-21 18:55:14 +0900

    feat: new header design done

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import HeaderLogo from '@/assets/images/ydevbook-logo.png';
+import HeaderNavDrawer from '@/components/molecules/HeaderNavDrawer';
 import HeaderProfileMenu from '@/components/molecules/HeaderProfileMenu';
 
 const navigation = [
@@ -8,23 +9,29 @@ const navigation = [
   { name: '스타트업 서비스', href: '/startup' },
 ];
 
+const drawerNavigation = [{ name: '메인페이지', href: '/' }, ...navigation];
+
 const Header = async () => {
   return (
     <header className="w-full bg-white shadow-sm">
       <div className="mx-auto max-w-[640px] px-4 h-[64px] flex justify-between items-center sm:max-w-7xl sm:px-10">
+        <HeaderNavDrawer navigation={drawerNavigation} />
         <Link href="/">
-          <div>
+          <div className="sm:hidden font-bold text-lg">YDevBook</div>
+          <div className="hidden sm:block">
             <Image alt="header logo" src={HeaderLogo} width={40} height={40} />
           </div>
         </Link>
-        <div className="hidden sm:flex items-center font-medium">
-          {navigation.map((item) => (
-            <Link key={item.name} href={item.href}>
-              <div className="text-gray-700 hover:font-bold mx-4 py-2">
-                {item.name}
-              </div>
-            </Link>
-          ))}
+        <div className="flex items-center">
+          <div className="hidden sm:flex items-center font-medium">
+            {navigation.map((item) => (
+              <Link key={item.name} href={item.href}>
+                <div className="text-gray-700 hover:font-bold mx-4 py-2">
+                  {item.name}
+                </div>
+              </Link>
+            ))}
+          </div>
           <HeaderProfileMenu
             menuNavigationsWhenLoggedIn={[
               {
