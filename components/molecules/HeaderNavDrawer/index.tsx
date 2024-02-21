412370d20dfab40c3b405e688e39fffacf7a38e7
Hyunwoo Seo <shw4008@gmail.com>
2024-02-21 18:58:09 +0900

    Feature: New Header (#43)
    * feat: new header base created
    
    * feat: new header desktop design done
    
    * feat: new header design done

---
+++ components/molecules/HeaderNavDrawer/index.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { RiCloseLine, RiMenuLine } from '@remixicon/react';
+import { Divider } from '@tremor/react';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { useState } from 'react';
+
+interface HeaderNavDrawerProps {
+  navigation: { name: string; href: string }[];
+}
+
+const HeaderNavDrawer = ({ navigation }: HeaderNavDrawerProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: session } = useSession();
+
+  return (
+    <div className="sm:hidden">
+      <button
+        onClick={() => setIsOpen(true)}
+        className="inline-flex items-center justify-center rounded-md bg-white p-1 -ml-1 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
+      >
+        <span className="sr-only">Open main menu</span>
+        <RiMenuLine className="h-6 w-6" />
+      </button>
+      <div
+        className={clsx(
+          isOpen ? 'block' : 'hidden',
+          'absolute top-0 left-0 bg-black/70 w-screen h-screen z-50'
+        )}
+      >
+        <div className="bg-white w-64 h-full p-4">
+          <div>
+            <button
+              onClick={() => setIsOpen(false)}
+              className="-ml-1 rounded-md bg-white text-gray-400 hover:bg-gray-100 hover:text-gray-500"
+            >
+              <RiCloseLine className="h-7 w-7" />
+            </button>
+          </div>
+          <div className="mt-12 space-y-4">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="block font-medium"
+                onClick={() => setIsOpen(false)}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <Divider />
+          <div className="space-y-4">
+            {session?.user ? (
+              <>
+                <Link href="/my-profile" onClick={() => setIsOpen(false)}>
+                  내 프로필
+                </Link>
+                <button
+                  onClick={async () => await signOut()}
+                  className="block font-medium"
+                >
+                  로그아웃
+                </button>
+              </>
+            ) : (
+              <button
+                onClick={async () => await signIn()}
+                className="block font-medium"
+              >
+                로그인
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderNavDrawer;
