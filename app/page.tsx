49fef9406d5962db5d5cd2674125bce66be58627
Hyunwoo Seo <shw4008@gmail.com>
2024-02-22 01:28:34 +0900

    Feature/main landing scaffolding (#47)
    * feat: add demo Main landing
    
    * feat: main page template height to min height, etc

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Text, Title, Button } from '@tremor/react';
+import { Card, Title } from '@tremor/react';
 import { sql } from '@vercel/postgres';
-import Link from 'next/link';
 
+import Link from 'next/link';
 import { auth } from '@/auth';
 import MainPageTemplate from '@/components/templates/MainPageTemplate';
 
@@ -20,12 +20,44 @@ export default async function IndexPage() {
 
   return (
     <MainPageTemplate>
-      <Title>메인 랜딩 페이지</Title>
-      <Text>서비스 소개 및 랜딩 디자인, 메인 CTA 생성</Text>
-      <div className="flex flex-col space-y-2.5">
-        <Link href={mainCTAHref}>
-          <Button>프로필 생성 / 마이 프로필(메인 CTA)</Button>
+      <div className="flex flex-col justify-center items-center my-24">
+        <h1 className="text-2xl font-bold  text-center whitespace-pre-line sm:text-4xl sm:leading-normal">
+          간편한 프로필 등록으로 <br />
+          유망한 스타트업의 연락을{'\n'}
+          받을 수 있는 기회!
+        </h1>
+        <p className="mt-8 text-lg text-gray-700 sm:text-xl">
+          대학생 개발자로서 스타트업에 합류해보세요.
+        </p>
+        <Link
+          href={mainCTAHref}
+          className="mt-8 px-8 py-2 bg-blue-500 text-white rounded-md"
+        >
+          {isLoggedIn && hasProfile ? '내 프로필 보기' : '프로필 등록하기'}
         </Link>
+      </div>
+      <div className="flex flex-col justify-center items-center my-24">
+        <div className="w-full px-10">
+          <p className="text-xl font-medium my-4">
+            스타트업에 나의 강점을 노출 시켜보세요.
+          </p>
+        </div>
+        <div className="px-10 w-full">
+          <Card>
+            <Title>나의 프로필 카드 예시</Title>
+            학력: 연세대학교 컴퓨터과학과 <br /> 키워드 1 | 키워드 2 | 키워드 3
+            <br />한 줄 소개 -----------
+          </Card>
+        </div>
+      </div>
+      <div className="flex flex-col items-center my-24">
+        <div className="w-[calc(100%-5rem)] h-[200px] bg-slate-600 rounded-md">
+          {/* <Image src={StartupImage} alt="startup image" layout="fill" /> */}
+          WIP 일러
+        </div>
+        <div className="mt-4">
+          <p>연뎁북은 한창 개발 중인 프로덕트입니다.</p>
+        </div>
       </div>
     </MainPageTemplate>
   );
--- components/templates/MainPageTemplate/index.tsx
+++ components/templates/MainPageTemplate/index.tsx
@@ -4,7 +4,7 @@ interface MainPageTemplateProps {
 
 const MainPageTemplate = ({ children }: MainPageTemplateProps) => {
   return (
-    <main className="p-4 sm:p-10 mx-auto max-w-7xl h-[calc(100%-64px)]">
+    <main className="p-4 sm:p-10 mx-auto max-w-7xl min-h-[calc(100%-64px)]">
       {children}
     </main>
   );
