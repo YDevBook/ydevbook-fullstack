593c08905c7dc75bd9c51290fef57948d6f180fe
Hyunwoo Seo <shw4008@gmail.com>
2024-02-23 17:45:08 +0900

    feat: 계정 신청 팝업 + 기타 디자인 수정사항

--- app/startup/login/page.tsx
+++ app/startup/login/page.tsx
@@ -4,9 +4,7 @@ import MobileOnlyTemplate from '@/components/templates/MobileOnlyTemplate';
 export default function StartupLoginPage() {
   return (
     <MobileOnlyTemplate>
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 md:-mt-32 p-4">
-        <LoginForm isStartup />
-      </div>
+      <LoginForm isStartup />
     </MobileOnlyTemplate>
   );
 }
